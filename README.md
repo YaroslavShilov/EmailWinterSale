6c616b3e0cd9ef6c11b66fade45c2f48cb2c9a6c
Yaroslav Shilov <workyarik@gmail.com>
2020-11-21 10:22:24 +0300

    update README

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Template for building responsive HTML Emails
+# Email Winter Sale
+[Application Link](https://yaroslavshilov.github.io/EmailWinterSale/)
+
+Using:
+1. pug
+2. scss
+3. zurb foundation
+4. gulp
--- gulpfile.js
+++ gulpfile.js
@@ -1,78 +1,79 @@
-const {src, dest, parallel, series, watch, task} = require('gulp')
+const { src, dest, parallel, series, watch, task } = require("gulp");
 
-const
-	sass         = require('gulp-sass'),
-	csso         = require('gulp-csso'),
-	cssnano      = require('gulp-cssnano'),
-	browserSync  = require('browser-sync'),
-	del          = require('del'),
-	autoprefixer = require('gulp-autoprefixer'),
-	pug					 = require('gulp-pug'),
-	inky 				 = require('inky'),
-	inlineCss		 = require('gulp-inline-css'),
-	inlineSource = require('gulp-inline-source');
+const sass = require("gulp-sass"),
+  csso = require("gulp-csso"),
+  cssnano = require("gulp-cssnano"),
+  browserSync = require("browser-sync"),
+  del = require("del"),
+  autoprefixer = require("gulp-autoprefixer"),
+  pug = require("gulp-pug"),
+  inky = require("inky"),
+  inlineCss = require("gulp-inline-css"),
+  inlineSource = require("gulp-inline-source"),
+  ghPages = require("gulp-gh-pages");
 
-task('browser-sync', function() {
-	browserSync({
-		server: {
-			baseDir: 'app'
-		},
-		notify: false
-	});
+task("deploy", function () {
+  return src("./dist/**/*").pipe(ghPages());
 });
 
-task('sass', function(){
-	return src('app/sass/main/**/*.+(scss|sass)')
-		.pipe(sass())
-		.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 10'], { cascade: true }))
-		.pipe(csso())
-		.pipe(cssnano())
-		.pipe(dest('app/css'))
-		.pipe(browserSync.reload({stream: true}))
+task("browser-sync", function () {
+  browserSync({
+    server: {
+      baseDir: "app",
+    },
+    notify: false,
+  });
 });
 
-task('inky', series('sass', function() {
-	return src('app/*.html')
-		.pipe(inky())
-		.pipe(dest('app/'))
-}));
-
-task('pug', function() {
-	return src('app/pug/pages/**/*.pug')
-		.pipe(pug({
-			pretty: true,
-		}))
-		.pipe(dest('app/'))
-		.pipe(browserSync.stream())
+task("sass", function () {
+  return src("app/sass/main/**/*.+(scss|sass)")
+    .pipe(sass())
+    .pipe(
+      autoprefixer(["last 15 versions", "> 1%", "ie 10"], { cascade: true })
+    )
+    .pipe(csso())
+    .pipe(cssnano())
+    .pipe(dest("app/css"))
+    .pipe(browserSync.reload({ stream: true }));
 });
 
+task(
+  "inky",
+  series("sass", function () {
+    return src("app/*.html").pipe(inky()).pipe(dest("app/"));
+  })
+);
 
-
-task('watch', function() {
-
-	watch('app/sass/**/*.+(scss|sass)', series('sass'));
-	watch('app/pug/**/*.pug', series('pug', 'inky'));
-
+task("pug", function () {
+  return src("app/pug/pages/**/*.pug")
+    .pipe(
+      pug({
+        pretty: true,
+      })
+    )
+    .pipe(dest("app/"))
+    .pipe(browserSync.stream());
 });
 
-
-
-task('build', async function() {
-
-	del.sync(['dist/*']);
-
-	let buildInky = src('app/*.html')
-		.pipe(inlineSource())
-		.pipe(inlineCss({
-			preserveMediaQueries: true,
-			removeLinkTags: true
-		}))
-		.pipe(dest('dist'));
-
-	let builImg = src('app/img/**/*')
-		.pipe(dest('dist/img'));
-
+task("watch", function () {
+  watch("app/sass/**/*.+(scss|sass)", series("sass"));
+  watch("app/pug/**/*.pug", series("pug", "inky"));
 });
 
+task("build", async function () {
+  del.sync(["dist/*"]);
 
-task('default', parallel('watch', 'browser-sync'));
+  let buildInky = src("app/*.html")
+    .pipe(inlineSource())
+    .pipe(
+      inlineCss({
+        preserveMediaQueries: true,
+        removeLinkTags: true,
+      })
+    )
+    .pipe(dest("dist"));
+
+  let builImg = src("app/img/**/*").pipe(dest("dist/img"));
+});
+
+task("default", parallel("watch", "browser-sync"));
